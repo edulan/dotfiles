902e6aaf6605db461489bad1ee56d4de758f5ff5
Eduardo Lanchares <edu@edu-XPS.(none)>
2015-04-05 17:13:37 +0200

    Symlink aliases

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.gemrc" ~
